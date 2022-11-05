be55437045294100e50264cd6b1a37daf443ad08
Yonka <yonka2003@gmail.com>
2022-11-05 15:19:05 +0200

    settings screen created

--- Resources/Resource.designer.cs
+++ Resources/Resource.designer.cs
@@ -4413,7 +4413,10 @@ namespace MathGame
 			public const int select_dialog_singlechoice_material = 2131427384;
 			
 			// aapt resource value: 0x7F0B0039
-			public const int support_simple_spinner_dropdown_item = 2131427385;
+			public const int settings_screen = 2131427385;
+			
+			// aapt resource value: 0x7F0B003A
+			public const int support_simple_spinner_dropdown_item = 2131427386;
 			
 			static Layout()
 			{
---
+++ SettingsActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathGame
+{
+    [Activity(Label = "SettingsActivity")]
+    public class SettingsActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
