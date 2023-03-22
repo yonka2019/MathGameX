094b656d859d6dfe2d90689bae7dbfd91bced776
עדן ליפשיץ <edenlivshits@outlook.com>
2023-03-22 18:36:07 +0200

    up

--- Activities/StatisticsActivity.cs
+++ Activities/StatisticsActivity.cs
@@ -6,6 +6,7 @@ using Android.Widget;
 using MathGame.Adapters;
 using Syncfusion.SfDataGrid;
 using System;
+using System.Collections.Generic;
 using System.Data;
 
 namespace MathGame.Activities
@@ -33,15 +34,34 @@ namespace MathGame.Activities
             dt.Columns.Add("Divide");
 
             dt.Rows.Add("asd", 0, 1, 2, 3);
+            var list = new List<Dictionary<string, string>>();
 
-            GridViewAdapter adapter = new GridViewAdapter(this, dt);
-            gv.Adapter = adapter;
+            foreach (DataRow row in dt.Rows)
+            {
+                var dictionary = new Dictionary<string, string>();
+                foreach (DataColumn column in dt.Columns)
+                {
+                    dictionary.Add(column.ColumnName, row[column].ToString());
+                }
+                list.Add(dictionary);
+            }
+
+            // Create a SimpleAdapter with the DataTable
+            var adapter = new SimpleAdapter(
+                this,
+                list,
+                Resource.Layout.list_item_layout,
+                new string[] { "Column 1", "Column 2", "Column 3" },
+                new int[] { Resource.Id.column1_textview, Resource.Id.column2_textview, Resource.Id.column3_textview });
+
+            // Set the adapter for the ListView
+            var listView = FindViewById<ListView>(Resource.Id.my_listview);
+            listView.Adapter = adapter;
         }
 
 
         private void SetRefs()
         {
-            gv = FindViewById<GridView>(Resource.Id.gridView1);
             backButton = FindViewById<Button>(Resource.Id.stats_backButton);
         }
 
